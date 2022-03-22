539753730fd2da9b32285a56df70cd6aee0371db
Lucas Nascimento Lopes <85919181+lucasnacimento@users.noreply.github.com>
2022-03-21 23:22:54 -0300

    Home page consuming api

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,55 +1,20 @@
-import './styles.css';
+import { Movie } from "../../types/movie";
+import { IMAGE_URL } from "../../config/request";
+import "./styles.css";
 
-function MovieCard() {
-    return (
-        <>
-        <ul className="list-movies row">
-            <li className="item-movie col-sm-6 col-lg-4 col-xl-2" >
-                <a href="https://google.com.br"><img src="https://cinema10.com.br/upload/filmes/filmes_9701_MV5BOTk5ODg0OTU5M15BMl5BanBnXkFtZTgwMDQ3MDY3NjM@._V1_SY1000_CR0,0,674,1000_AL_.jpg?default=poster" alt="aquaman" /></a>
-                <span>Aquaman</span>
-            </li>
-            <li className="item-movie col-sm-6 col-lg-4 col-xl-2" >
-                <a href="https://google.com.br"><img src="https://cinema10.com.br/upload/filmes/filmes_9701_MV5BOTk5ODg0OTU5M15BMl5BanBnXkFtZTgwMDQ3MDY3NjM@._V1_SY1000_CR0,0,674,1000_AL_.jpg?default=poster" alt="aquaman" /></a>
-                <span>Aquaman</span>
-            </li>
-            <li className="item-movie col-sm-6 col-lg-4 col-xl-2" >
-                <a href="https://google.com.br"><img src="https://cinema10.com.br/upload/filmes/filmes_9701_MV5BOTk5ODg0OTU5M15BMl5BanBnXkFtZTgwMDQ3MDY3NjM@._V1_SY1000_CR0,0,674,1000_AL_.jpg?default=poster" alt="aquaman" /></a>
-                <span>Aquaman</span>
-            </li>
-            <li className="item-movie col-sm-6 col-lg-4 col-xl-2" >
-                <a href="https://google.com.br"><img src="https://cinema10.com.br/upload/filmes/filmes_9701_MV5BOTk5ODg0OTU5M15BMl5BanBnXkFtZTgwMDQ3MDY3NjM@._V1_SY1000_CR0,0,674,1000_AL_.jpg?default=poster" alt="aquaman" /></a>
-                <span>Aquaman</span>
-            </li>
-            <li className="item-movie col-sm-6 col-lg-4 col-xl-2" >
-                <a href="https://google.com.br"><img src="https://cinema10.com.br/upload/filmes/filmes_9701_MV5BOTk5ODg0OTU5M15BMl5BanBnXkFtZTgwMDQ3MDY3NjM@._V1_SY1000_CR0,0,674,1000_AL_.jpg?default=poster" alt="aquaman" /></a>
-                <span>Aquaman</span>
-            </li>
-            <li className="item-movie col-sm-6 col-lg-4 col-xl-2" >
-                <a href="https://google.com.br"><img src="https://cinema10.com.br/upload/filmes/filmes_9701_MV5BOTk5ODg0OTU5M15BMl5BanBnXkFtZTgwMDQ3MDY3NjM@._V1_SY1000_CR0,0,674,1000_AL_.jpg?default=poster" alt="aquaman" /></a>
-                <span>Aquaman</span>
-            </li>
-            <li className="item-movie col-sm-6 col-lg-4 col-xl-2" >
-                <a href="https://google.com.br"><img src="https://cinema10.com.br/upload/filmes/filmes_9701_MV5BOTk5ODg0OTU5M15BMl5BanBnXkFtZTgwMDQ3MDY3NjM@._V1_SY1000_CR0,0,674,1000_AL_.jpg?default=poster" alt="aquaman" /></a>
-                <span>Aquaman</span>
-            </li>
-            <li className="item-movie col-sm-6 col-lg-4 col-xl-2" >
-                <a href="https://google.com.br"><img src="https://cinema10.com.br/upload/filmes/filmes_9701_MV5BOTk5ODg0OTU5M15BMl5BanBnXkFtZTgwMDQ3MDY3NjM@._V1_SY1000_CR0,0,674,1000_AL_.jpg?default=poster" alt="aquaman" /></a>
-                <span>Aquaman</span>
-            </li>
-            <li className="item-movie col-sm-6 col-lg-4 col-xl-2" >
-                <a href="https://google.com.br"><img src="https://cinema10.com.br/upload/filmes/filmes_9701_MV5BOTk5ODg0OTU5M15BMl5BanBnXkFtZTgwMDQ3MDY3NjM@._V1_SY1000_CR0,0,674,1000_AL_.jpg?default=poster" alt="aquaman" /></a>
-                <span>Aquaman</span>
-            </li>
-            <li className="item-movie col-sm-6 col-lg-4 col-xl-2" >
-                <a href="https://google.com.br"><img src="https://cinema10.com.br/upload/filmes/filmes_9701_MV5BOTk5ODg0OTU5M15BMl5BanBnXkFtZTgwMDQ3MDY3NjM@._V1_SY1000_CR0,0,674,1000_AL_.jpg?default=poster" alt="aquaman" /></a>
-                <span>Aquaman</span>
-            </li>
-            
-           
-        </ul>
-        </>
-    )
+type Props = {
+  movie: Movie;
+};
 
+function MovieCard({ movie }: Props) {
+  return (
+    <>
+      <a href="https://google.com.br">
+        <img src={`${IMAGE_URL}${movie.poster_path}`} alt="aquaman" />
+      </a>
+      <span>{movie.title}</span>
+    </>
+  );
 }
 
 export default MovieCard;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,16 +1,54 @@
 import Footer from "../../components/Footer";
 import MovieCard from "../../components/MovieCard";
 import Navbar from "../../components/Navbar";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { URL_BASE_POPULAR, KEY_API, LANGUAGE_CODE } from '../../config/request';
+import { MoviePage } from "../../types/movie";
 
 function Home() {
+
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const [page, setPage] = useState<MoviePage>({
+    page: pageNumber,
+    results: [],
+    total_pages: 0,
+    total_results: 0,
+    qtdItemsFromPage: 20,
+    beforePage: pageNumber-1,
+    laterPage: pageNumber+1
+  });
+
+  useEffect(() => {
+    axios
+      .get(`${URL_BASE_POPULAR}${KEY_API}&${LANGUAGE_CODE}&page=${pageNumber}`)
+      .then((response) => {
+        const data = response.data as MoviePage;
+        setPage(data);
+      });
+      
+      console.log(page.results);
+    }, [pageNumber]);
+
+  const handlePageChange = (newPageNumber: number) => {
+    setPageNumber(newPageNumber);
+  }
+
   return (
     <>
       <Navbar />
         <div className="d-flex justify-content-center mt-5">
-          <h1>Movies</h1>
+          <h1>Filmes</h1>
         </div>
-        <MovieCard />
+        <div className="list-movies row ">
+        {page.results.map((movie) => (
+            <li key={movie.id} className="item-movie col-sm-6 col-lg-4 col-xl-2" >
+              <MovieCard movie={movie} />
+            </li>
+          ))}
+
+        </div>
       <Footer/>
     </>
   );
